9f9d983fe78678380cd5839bac5bb9031ade3511
fentybit <fentymhall@gmail.com>
2021-05-19 18:10:13 -0700

    add more database to seeding file

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,7 +17,7 @@ user = User.create(username: 'fentybit', password: 'fentybit', firstname: 'Fenty
 #! DOG 
 first_category = Category.create(name: 'Dog')
 
-event_1 = Event.create(title: 'Fun Day at the Dog Park', vibe: '1 rad', date: '2021-05-10', time: '2021-05-10 12:00:00', location: '12604 Bluff Creek Dr, Los Angeles, CA 90094', latitude: '33.9752466', longitude: '-118.4156689', description: 'Dog having too much fun, and he could not control himself. Maybe I need to take him to another dUser.og park to see how he is doing, he pooped twice.', category_id: first_category.id, user_id: user.id)
+event_1 = Event.create(title: 'Fun Day at the Dog Park', vibe: '1 rad', date: '2021-04-10', time: '2021-04-10 12:00:00', location: '12604 Bluff Creek Dr, Los Angeles, CA 90094', latitude: '33.9752466', longitude: '-118.4156689', description: 'Dog having too much fun, and he could not control himself. Maybe I need to take him to another dUser.og park to see how he is doing, he pooped twice.', category_id: first_category.id, user_id: user.id)
 
 Image.create(url: 'https://res.cloudinary.com/dvxp6coec/image/upload/v1621219888/wzjhdhotbrtbomiqzawe.png', event_id: event_1.id)
 
@@ -29,7 +29,7 @@ Image.create(url: 'https://res.cloudinary.com/dvxp6coec/image/upload/v1621258671
 #! FOOD
 second_category = Category.create(name: 'Food')
 
-event_3 = Event.create(title: 'Birthday Din Din!', vibe: '1 rad', date: '2021-05-01', time: '2021-05-01 18:00:00', location: 'Lawry\'s', latitude: '33.9158844', longitude: '-118.413313', description: 'I love cheese, especially airedale queso. Cheese and biscuits halloumi cauliflower cheese cottage cheese swiss boursin fondue caerphilly. Cow port-salut camembert de normandie macaroni cheese feta who moved my cheese babybel boursin. Red leicester roquefort boursin squirty cheese jarlsberg blue castello caerphilly chalk and cheese. Lancashire.', category_id: second_category.id, user_id: user.id)
+event_3 = Event.create(title: 'Birthday Din Din!', vibe: '1 rad', date: '2021-04-25', time: '2021-04-25 18:00:00', location: 'Lawry\'s', latitude: '33.9158844', longitude: '-118.413313', description: 'I love cheese, especially airedale queso. Cheese and biscuits halloumi cauliflower cheese cottage cheese swiss boursin fondue caerphilly. Cow port-salut camembert de normandie macaroni cheese feta who moved my cheese babybel boursin. Red leicester roquefort boursin squirty cheese jarlsberg blue castello caerphilly chalk and cheese. Lancashire.', category_id: second_category.id, user_id: user.id)
 
 Image.create(url: 'https://res.cloudinary.com/dvxp6coec/image/upload/v1621258942/birthday_dinner.png', event_id: event_3.id)
 
@@ -41,7 +41,7 @@ event_5 = Event.create(title: 'Garlic Noodles', vibe: '4 bad', date: '2021-05-19
 
 Image.create(url: 'https://res.cloudinary.com/dvxp6coec/image/upload/v1621258675/garlic_noodles.png', event_id: event_5.id)
 
-event_6 = Event.create(title: 'Date night at Mercado', vibe: '5 awful', date: '2021-05-21', time: '2021-05-21 19:00:00', location: '3212A N Sepulveda Blvd, Manhattan Beach, CA 90266', latitude: '33.8992204', longitude: '-118.3962446', description: 'Zombie ipsum reversus ab viral inferno, nam rick grimes malum cerebro. De carne lumbering animata corpora quaeritis. Summus brains sit , morbo vel maleficia? De apocalypsi gorger omero undead survivor dictum mauris. Hi mindless mortuis soulless creaturas, imo evil stalking monstra adventus resi dentevil vultus comedat cerebella viventium.', category_id: second_category.id, user_id: user.id)
+event_6 = Event.create(title: 'Date night at Mercado', vibe: '5 awful', date: '2021-05-22', time: '2021-05-22 19:00:00', location: '3212A N Sepulveda Blvd, Manhattan Beach, CA 90266', latitude: '33.8992204', longitude: '-118.3962446', description: 'Zombie ipsum reversus ab viral inferno, nam rick grimes malum cerebro. De carne lumbering animata corpora quaeritis. Summus brains sit , morbo vel maleficia? De apocalypsi gorger omero undead survivor dictum mauris. Hi mindless mortuis soulless creaturas, imo evil stalking monstra adventus resi dentevil vultus comedat cerebella viventium.', category_id: second_category.id, user_id: user.id)
 
 Image.create(url: 'https://res.cloudinary.com/dvxp6coec/image/upload/v1621258942/birthday_dinner.png', event_id: event_6.id)
 
@@ -49,7 +49,7 @@ Image.create(url: 'https://res.cloudinary.com/dvxp6coec/image/upload/v1621258942
 #! TRAVEL
 third_category = Category.create(name: 'Travel')
 
-event_7 = Event.create(title: 'Anniversary Trip, woot!', vibe: '1 rad', date: '2021-05-03', time: '2021-05-03 09:00:00', location: 'San Diego, CA', latitude: '32.8242404', longitude: '-117.3891598', description: 'Secondary fermentation degrees plato units of bitterness, cask conditioned ale ibu real ale pint glass craft beer. krausen goblet grainy ibu brewhouse lagering finishing hops. Trappist, black malt chocolate malt balthazar gravity dextrin saccharification trappist final gravity. Aau scotch ale, adjunct. hops bung infusion, cask conditioning pitching malt extract.', category_id: third_category.id, user_id: user.id)
+event_7 = Event.create(title: 'Anniversary Trip, woot!', vibe: '1 rad', date: '2021-05-02', time: '2021-05-02 09:00:00', location: 'Canyonlands National Park', latitude: '38.2256276', longitude: '-110.4596296', description: 'Secondary fermentation degrees plato units of bitterness, cask conditioned ale ibu real ale pint glass craft beer. krausen goblet grainy ibu brewhouse lagering finishing hops. Trappist, black malt chocolate malt balthazar gravity dextrin saccharification trappist final gravity. Aau scotch ale, adjunct. hops bung infusion, cask conditioning pitching malt extract.', category_id: third_category.id, user_id: user.id)
 
 Image.create(url: 'https://res.cloudinary.com/dvxp6coec/image/upload/v1621258679/trip.png', event_id: event_7.id)
 
@@ -57,5 +57,15 @@ event_8 = Event.create(title: 'Sharan\'s Bachelorette Party', vibe: '2 good', da
 
 Image.create(url: 'https://res.cloudinary.com/dvxp6coec/image/upload/v1621258740/bachelorette.png', event_id: event_8.id)
 
+#! YOGA
+fourth_category = Category.create(name: 'Yoga')
+
+event_9 = Event.create(title: 'Favorite Yoga poses', vibe: '2 good', date: '2021-04-20', time: '2021-04-20 12:00:00', location: 'Denver, CO', latitude: '39.7640005', longitude: '-105.1359767', description: 'Heart-opener mindfulness out of your comfort zone paschimottanasana the crown of your head mudra Om ground down heart-opener. Transform breath of fire exhale, exhale hug in the light within me namaste chaturanga. Ground down exhale bandha exhale. Karma bikram hug in mind exhale heart center balance! Flow expand out, rejuvenate rejuvenate the light within me, exhale equanimity asana. Shoulder integration asana balance all ten toes exhale all ten toes downward-facing dog inhale.', category_id: fourth_category.id, user_id: user.id)
+
+Image.create(url: 'https://res.cloudinary.com/dvxp6coec/image/upload/v1621471452/yoga.png', event_id: event_9.id)
+
+event_10 = Event.create(title: 'Yoga on Sunset Beach', vibe: '1 rad', date: '2021-05-01', time: '2021-05-01 19:00:00', location: 'Ocean Beach, CA', latitude: '37.7772136', longitude: '-122.5092722', description: 'Bacon ipsum dolor amet short ribs brisket venison rump drumstick pig sausage prosciutto chicken spare ribs salami picanha doner. Kevin capicola sausage, buffalo bresaola venison turkey shoulder picanha ham pork tri-tip meatball meatloaf ribeye. Doner spare ribs andouille bacon sausage. Ground round jerky brisket pastrami shank.', category_id: fourth_category.id, user_id: user.id)
+
+Image.create(url: 'https://res.cloudinary.com/dvxp6coec/image/upload/v1621472938/beach_yoga.png', event_id: event_10.id)
 
 puts 'SEEDING SUCCESS!'
